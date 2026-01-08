d53cab1e942f84d647ee50fd3351c3c655708d43
Mnthn <107455609+manthan127@users.noreply.github.com>
2026-01-08 07:08:34 +0000

    initial commit

---
+++ Sources/UtilsPropertyWrappers/DecodedFromString.swift
@@ -0,0 +1,54 @@
+//
+//  DecodedFromString.swift
+//  Utils
+//
+//  Created by Home on 17/04/25.
+//
+
+import Foundation
+
+@propertyWrapper struct DecodedFromString<Value: LosslessStringConvertible> {
+    private var value: Value?
+    
+    init(wrappedValue: Value?) {
+        self.value = wrappedValue
+    }
+    
+    var wrappedValue: Value? {
+        get {
+            value
+        }  set {
+            value = newValue
+        }
+    }
+}
+
+extension DecodedFromString: Decodable where Value: Decodable {
+    init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        
+        if let stringValue = try? container.decode(String.self),
+           let parsedValue = Value(stringValue) {
+            self.value = parsedValue
+        } else if let directValue = try? container.decode(Value.self) {
+            self.value = directValue
+        } else {
+            self.value = nil
+        }
+    }
+}
+
+extension DecodedFromString: Encodable where Value: Encodable { }
+
+
+//struct Example: LosslessStringConvertible, Decodable {
+//    init?(_ description: String) {
+//        if let x =  try? description.decode(to: Self.self) {
+//            self = x
+//        } else {
+//            return nil
+//        }
+//    }
+//    
+//    var description: String
+//}
