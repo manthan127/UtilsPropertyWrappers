e2b90c452c4d6040fd7c8ab1f29da07f0c3c858d
Mnthn <107455609+manthan127@users.noreply.github.com>
2026-04-01 19:30:32 +0530

    Refactor AppStorageCodable to use DynamicProperty

--- Sources/UtilsPropertyWrappers/AppStorageCodable.swift
+++ Sources/UtilsPropertyWrappers/AppStorageCodable.swift
@@ -11,31 +11,36 @@ import Foundation
 //  if two different place are using same key both variable should be in sink
 
 // will not update UI if used from @ObsevableObject
-// need to test for directly in View
 // might need to use Combine since @Publised and @ObsevableObject are in Combine framework
 @propertyWrapper
-public struct AppStorageCodable<Value: Codable> {
+public struct AppStorageCodable<Value: Codable>: DynamicProperty {
     let key: String
-    var value: Value
+    @State private var value: Value
+    private let store: UserDefaults
     
-    public init(wrappedValue: Value, key: String) {
+    public init(wrappedValue: Value, _ key: String, store: UserDefaults? = nil) {
         self.key = key
-        
-        let value = try? UserDefaults.standard.data(forKey: key)?.jsonDecoded(Value.self)
-        self.value = value ?? wrappedValue
+        self.store = store ?? UserDefaults.standard
+        let value = try? self.store.data(forKey: key)?.jsonDecoded(Value.self)
+        self._value = .init(wrappedValue: value ?? wrappedValue)
     }
     
     public var wrappedValue: Value {
         get {
             value
-        } set {
+        } nonmutating set {
             if let data = try? newValue.jsonEncoded() {
                 value = newValue
-                UserDefaults.standard.set(data, forKey: key)
+                store.set(data, forKey: key)
             }
         }
     }
+    
+    public var projectedValue: Binding<Value> {
+        .init { wrappedValue } set: { wrappedValue = $0 }
+    }
 }
+
 
 extension Data {
     func jsonDecoded<D: Decodable>(_ type: D.Type, decoder: JSONDecoder = JSONDecoder()) throws -> D {
