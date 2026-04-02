6b3dfa37c807cccc3f80f7dda23cc6fc8216ab9a
Mnthn <107455609+manthan127@users.noreply.github.com>
2026-04-02 10:04:01 +0530

    sync value in AppStorageCodable
    Sync data if there AppStorageCodable is used at two places at same time

--- Sources/UtilsPropertyWrappers/AppStorageCodable.swift
+++ Sources/UtilsPropertyWrappers/AppStorageCodable.swift
@@ -6,41 +6,72 @@
 //
 
 import Foundation
+import Combine
 
 //    @AppStorage is some how updating in directly view and from @ObsevableObject
-//  if two different place are using same key both variable should be in sink
 
 // will not update UI if used from @ObsevableObject
 // might need to use Combine since @Publised and @ObsevableObject are in Combine framework
 @propertyWrapper
 public struct AppStorageCodable<Value: Codable>: DynamicProperty {
-    let key: String
-    @State private var value: Value
-    private let store: UserDefaults
+    @StateObject private var storage: CodableStorage<Value>
     
-    public init(wrappedValue: Value, _ key: String, store: UserDefaults? = nil) {
-        self.key = key
-        self.store = store ?? UserDefaults.standard
-        let value = try? self.store.data(forKey: key)?.jsonDecoded(Value.self)
-        self._value = .init(wrappedValue: value ?? wrappedValue)
+    public init(wrappedValue: Value, _ key: String) {
+        _storage = StateObject(
+            wrappedValue: CodableStorage(key: key, defaultValue: wrappedValue)
+        )
     }
     
     public var wrappedValue: Value {
-        get {
-            value
-        } nonmutating set {
-            if let data = try? newValue.jsonEncoded() {
-                value = newValue
-                store.set(data, forKey: key)
-            }
-        }
+        get { storage.value }
+        nonmutating set { storage.set(newValue) }
     }
     
     public var projectedValue: Binding<Value> {
-        .init { wrappedValue } set: { wrappedValue = $0 }
+        .init { storage.value } set: { storage.set($0) }
     }
 }
 
+private final class CodableStorage<Value: Codable>: ObservableObject {
+    @Published var value: Value
+    
+    private let key: String
+    private let store: UserDefaults
+    
+    init(key: String, defaultValue: Value, store: UserDefaults = .standard) {
+        self.key = key
+        self.store = store
+        
+        if let data = store.data(forKey: key),
+           let decoded = try? data.jsonDecoded(Value.self) {
+            self.value = decoded
+        } else {
+            self.value = defaultValue
+        }
+        // Might be issue when model is very big and there are to many observers
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(sync),
+            name: UserDefaults.didChangeNotification,
+            object: store
+        )
+    }
+    @objc private func sync() {
+        if let data = store.data(forKey: key),
+           let decoded = try? data.jsonDecoded(Value.self) {
+            // TODO: only update value if value at the key is changed
+            // TODO: do not change if value is not changed
+            value = decoded
+        }
+    }
+    
+    func set(_ newValue: Value) {
+        value = newValue
+        if let data = try? newValue.jsonEncoded() {
+            store.set(data, forKey: key)
+        }
+    }
+}
 
 extension Data {
     func jsonDecoded<D: Decodable>(_ type: D.Type, decoder: JSONDecoder = JSONDecoder()) throws -> D {
